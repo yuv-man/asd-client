5748e49e23ee37fbd675722e5b25bc4c3532c20c
Yuval Mandler <yuvalmandler@gmail.com>
2025-04-25 12:54:17 +0300

    fix home

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,15 +2,31 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { sessionStore, useUserStore } from '@/store/userStore';
-import { useTranslations } from 'next-intl';  
+import { sessionStore, useUserStore } from '@/store/userStore'; 
+import { getSession } from 'next-auth/react';
+import { userAPI } from '@/lib/api';
 import '@/app/styles/mainPage.scss';
 
 export default function Home() {
   const router = useRouter();
   const user = useUserStore((state) => state.user);
   const [isHydrated, setIsHydrated] = useState(false);
-  const t = useTranslations();
+
+  const checkUser = async () => {
+    if (user) {
+      sessionStore.initializeSessions();
+      router.push('/training');
+    } else {
+      const session = await getSession();
+      const userResponse = await userAPI.getByEmail(session?.user?.email || '');
+      if (userResponse) {
+        sessionStore.initializeSessions();
+        router.push('/training');
+      } else {
+        router.push('/signup');
+      }
+    }
+  }
 
   useEffect(() => {
     if (user !== undefined) {
@@ -20,13 +36,7 @@ export default function Home() {
 
   useEffect(() => {
     if (!isHydrated) return;
-
-    if (user) {
-      sessionStore.initializeSessions();
-      router.push('/training');
-    } else {
-      router.push('/signup');
-    }
+    checkUser();
   }, [isHydrated, user, router, sessionStore]);
 
   return <div className="home-container" />;
